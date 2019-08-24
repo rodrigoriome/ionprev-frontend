04bedab3c38886f34c5df7f846625414ce1fe8c6
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-08-23 21:24:33 -0300

    fix api config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.VUE_APP_API_HOST}:${process.env.VUE_APP_API_PORT}`
+  baseURL:
+    parseInt(process.env.VUE_APP_API_PORT) === 80
+      ? process.env.VUE_APP_API_HOST
+      : `${process.env.VUE_APP_API_HOST}:${process.env.VUE_APP_API_PORT}`
 });
 
 export default api;
